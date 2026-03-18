cee29ce4f35a0940af2fc07aa380b26a07be612e
minnie-star <minenhleminnie8@gmail.com>
2026-03-18 20:44:47 +0200

    package.json update

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 // main.js
 import ProductList from "./productList.mjs";
-import ExternalDataSource from "./ExternalDataSource.mjs";
+import ProductData from "./ProductData.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 const listElement = document.querySelector("#product-list");
-const dataSource = new ExternalDataSource();
+const dataSource = new ProductData();
 
 const productList = new ProductList("electronics", dataSource, listElement);
 productList.init();
