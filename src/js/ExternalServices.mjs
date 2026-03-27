3d47df00a8f25e67081b6c549386180dfcb4d5c3
Daniel Benjamin chinonso iwuchukwu <benjaminsoosayiwuchukwu@gmail.com>
2026-03-27 10:24:29 -0700

    Change API base URL to HTTPS to fix mixed content error

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,4 +1,5 @@
 const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
+
 async function convertToJson(res) {
   const data = await res.json();
   if (res.ok) {
@@ -9,10 +10,6 @@ async function convertToJson(res) {
 }
 
 export default class ExternalServices {
-  constructor(category) {
-    // this.category = category;
-    // this.path = `../json/${this.category}.json`;
-  }
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
